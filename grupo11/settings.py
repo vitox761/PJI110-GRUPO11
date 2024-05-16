266d33e0a55b3e9177ecdc20814b1738041d0178
JOSE CRISTINO DA SILVA JUNIOR <165227843+JoseChristino@users.noreply.github.com>
2024-05-15 21:32:23 -0300

    static files

--- grupo11/settings.py
+++ grupo11/settings.py
@@ -125,9 +125,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = os.environ.get("DJANGO_STATIC_URL", "/static/")
+STATIC_URL = os.environ.get("DJANGO_STATIC_URL", "static/")
 STATIC_ROOT = os.environ.get("DJANGO_STATIC_ROOT", "./static/")
-STATICFILES_DIRS = [os.path.join('build', 'static')] 
 STATICFILES_STORAGE = ('whitenoise.storage.CompressedManifestStaticFilesStorage')
 
 # Default primary key field type
